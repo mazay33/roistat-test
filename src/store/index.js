a8d7895fc105fdaff8b8536c3150c3ab69b1002e
Mazaev Andrey <mazaev2000@gmail.com>
2023-03-28 22:23:24 +0300

    feat: data for table

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,18 @@ import { createStore } from 'vuex';
 
 const store = createStore({
   state: {
-    
+    tableData: [
+      { name: 'Мария', number: "+7 987 654 32 10", children: [
+        { name: 'Саша', number: "+7 123 456 78 90", children: [
+          { name: 'Андрей', number: "+7 977 341 75 08" }
+        ]}
+      ]},
+      { name: 'Анна', number: "+7 987 654 32 10", children: [
+        { name: 'Сергей', number: "+7 123 456 78 90" },
+        { name: 'Георгий', number: "+7 987 654 32 10" }
+      ]},
+      { name: 'Владислав', number: "+7 123 456 78 90" }
+    ],
   },
   getters: {
 
